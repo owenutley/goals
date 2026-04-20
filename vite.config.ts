a3bbffa77bb284b36d2e4c3ff7f1d6c5b5c17500
Owen Utley <owen.utley17@gmail.com>
2026-04-20 15:54:53 -0600

    syntax fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/goals/,
+    base: '/goals/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
